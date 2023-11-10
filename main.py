a34d980adf09cc2ed49ea17810cf917971ae2821
Jonathan Stanczak <tul50549@temple.edu>
2023-11-09 21:41:30 -0500

    Moved menu.py function to main
    Moved button function to main to implement quick fix for release. Will be reverted and changed at a later date

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #main will call the other files and classes to run the game
 
 import pygame
-import menu
+# import menu # import menu.py file, currently disabled
 
 # create pygame window  and set title
 pygame.init()
@@ -27,7 +27,7 @@ pygame.mixer.music.play(-1)
 
 # title for display 
 game_title = "Checkers+"
-message = "Checkers with a twist! For all ages and skill levels!" # can change to whatever we feel is best
+message = "Checkers with a twist! For all ages and skill levels!"
 
 background_image = pygame.image.load("checkers.jpg")
 background_image = pygame.transform.scale(background_image, (Width, Height))  
@@ -64,12 +64,75 @@ def main():
         screen.blit(message_text, message_rect)
         
         # call PvP button function from menu.py
-        menu.pvp_button()
+        menu_buttons()
 
         # flip the display
         pygame.display.flip()
 
     # done! time to quit
     pygame.quit()
+
+# function to create menu buttons
+# currently supports PvP and settings buttons
+def menu_buttons():
+    color = (128, 128, 128) # grey
+    cursor_color = (100, 100, 100) # darker grey
+    position = (Width // 2-150, Height // 3-25)
+    size = (300, 50)  # width, height
+        
+    button_font = pygame.font.Font(None, 32)
+    button_text = button_font.render("Start Game Against Player", True, (255, 255, 255)) # Button text and color
+    button_text_rect = button_text.get_rect(center=(Width // 2, Height // 3))
+    
+    # Create button on screen using position and size parameters
+    pygame.draw.rect(screen, color, pygame.Rect(position, size))
+    screen.blit(button_text, button_text_rect)
+    
+    # Used to indicate if cursor is hovering over button. If so, button will be darker
+    mouse = pygame.mouse.get_pos()
+    button_rect = pygame.Rect(position, size)
+    if button_rect.collidepoint(mouse):
+        pygame.draw.rect(screen, cursor_color, button_rect)  # Change color when cursor hovered over
+    else:
+        pygame.draw.rect(screen, color, button_rect) # stay original color if cursor not hovering over
+
+    screen.blit(button_text, button_text_rect)
+    
+    # Settings Button    
+    settings_icon = pygame.image.load('pics/settings_icon.png')
+
+    button_height = 50
+    spacing = 10
+
+    position = (Width // 2 - 150, Height // 3 + button_height + spacing)
+    size = (300, button_height)  # width, height
+
+    button_text = button_font.render("Settings", True, (255, 255, 255))  # Button text and color
+    button_text_rect = button_text.get_rect(center=(Width // 2, Height // 3 + button_height + spacing + button_height // 2))
+
+    # Set the desired size for the icon
+    icon_size = (45, 45)  # Adjust the size of the icon as needed
+
+    # Draw the icon next to the text with the specified size
+    settings_icon_resized = pygame.transform.scale(settings_icon, icon_size)
+    settings_icon_rect = settings_icon_resized.get_rect(topleft=(Width // 2 - 150 + 10, Height // 3 + button_height + spacing + (button_height - icon_size[1]) // 2))
+
+    # Create button on screen using position and size parameters
+    pygame.draw.rect(screen, color, pygame.Rect(position, size))
+    screen.blit(button_text, button_text_rect)
+
+    # Used to indicate if the cursor is hovering over the button. If so, the button will be darker
+    mouse = pygame.mouse.get_pos()
+    button_rect_2 = pygame.Rect(position, size)
+    if button_rect_2.collidepoint(mouse):
+        pygame.draw.rect(screen, cursor_color, button_rect_2)  # Change color when cursor hovered over
+    else:
+        pygame.draw.rect(screen, color, button_rect_2)  # Stay the original color if the cursor is not hovering over
+
+    screen.blit(settings_icon_resized, settings_icon_rect.topleft)  # Draw the icon after drawing the button
+    screen.blit(button_text, button_text_rect)
+
+    return button_rect, button_rect_2
+
 
 main()
