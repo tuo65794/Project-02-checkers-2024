4805ca1c0fcd2bd7649a5725b19ef97d4ef410d5
NathanMcCourt <tuk91355@temple.edu>
2023-11-29 15:23:32 -0500

    Updated constants

--- constants.py
+++ constants.py
@@ -1,4 +1,13 @@
+import pygame
+# used for color customization and more (not all may be used)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+WIDTH, HEIGHT = 1000,1000
 GREY = (128,128,128)
+GREEN = (0,128,0)
+BROWN = (150, 75, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+ROWS, COLS = 8, 8
+SQUARE_SIZE = 85
